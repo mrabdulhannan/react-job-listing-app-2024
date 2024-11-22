c90269e461f7801b73c502c526c11226c8744429
abdulhannandanish <mrahdanish@gmail.com>
2024-11-22 19:01:09 +0500

    Add Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,33 @@
+import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom';
 import React from 'react';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import HomeCards from './components/HomeCards';
-import JobListings from './components/JobListings';
-import ViewAllJobs from './components/ViewAllJobs';
+import HomePage from './pages/HomePage';
+
+import MainLayout from './layouts/MainLayout';
 
 const App = () => {
-  
-  return (
-    <>
-    < Navbar />
-    <Hero />
-    <HomeCards />
-    <JobListings/>
-   <ViewAllJobs/>
-   
-    </>
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+    <Route path='/' element={<MainLayout/>}>
+      <Route index element={< HomePage />} />
+    </Route>
+    
   )
+);
+
+  return <RouterProvider router={router} />;
+
 }
 
 export default App
 
 {/* Time 1:07:03 https://www.youtube.com/watch?v=LDB4uaJ87e0&t=1401s */}
+  // return (
+  //   <>
+  //   < Navbar />
+  //   <Hero />
+  //   <HomeCards />
+  //   <JobListings/>
+  //  <ViewAllJobs/>
+   
+  //   </>
+  // )
